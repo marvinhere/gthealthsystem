4eb868a92074181b1dcabf24e5b9ca42700d24cb
marvinhere <62631321+marvinhere@users.noreply.github.com>
2020-03-24 21:56:25 -0600

    First Version

---
+++ js/init.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+   apiKey: "AIzaSyC52FTtS4ijsgr92tMiHdmkPZSY2Hqv_HU",
+    authDomain: "social-a2299.firebaseapp.com",
+    databaseURL: "https://social-a2299.firebaseio.com",
+    projectId: "social-a2299",
+    storageBucket: "social-a2299.appspot.com",
+    messagingSenderId: "747821466643",
+    appId: "1:747821466643:web:6ce4b9b7d3201c42b62be6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
