e6c0eb6ccc0d2327eb483a86c25da42838ed4e9c
marvinhere <62631321+marvinhere@users.noreply.github.com>
2020-03-24 23:08:37 -0600

    Change credentials

--- js/init.js
+++ js/init.js
@@ -1,12 +1,13 @@
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-   apiKey: "AIzaSyC52FTtS4ijsgr92tMiHdmkPZSY2Hqv_HU",
-    authDomain: "social-a2299.firebaseapp.com",
-    databaseURL: "https://social-a2299.firebaseio.com",
-    projectId: "social-a2299",
-    storageBucket: "social-a2299.appspot.com",
-    messagingSenderId: "747821466643",
-    appId: "1:747821466643:web:6ce4b9b7d3201c42b62be6"
+var firebaseConfig = {
+    apiKey: "AIzaSyCWmc2I5wN16sEYQ0l-oMSKsVgqdinoGvI",
+    authDomain: "gthealthsystem.firebaseapp.com",
+    databaseURL: "https://gthealthsystem.firebaseio.com",
+    projectId: "gthealthsystem",
+    storageBucket: "gthealthsystem.appspot.com",
+    messagingSenderId: "233767847166",
+    appId: "1:233767847166:web:4fddb1a6c6ed2543f651af",
+    measurementId: "G-5GSTEHQ9BT"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
